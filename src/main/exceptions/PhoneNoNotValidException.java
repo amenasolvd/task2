9b72dac471013dfe40569160c5bc293e3bbf6c4a
amenasolvd <akureshi.laba@solvd.com>
2023-11-17 17:38:16 -0500

    changes done according to comments

--- src/main/exceptions/PhoneNoNotValidException.java
+++ src/main/exceptions/PhoneNoNotValidException.java
@@ -1,7 +1,5 @@
 package main.exceptions;
 
-import org.apache.logging.log4j.Logger;
-
 public class PhoneNumberNotValid extends Exception{
     public PhoneNumberNotValid(String e){
         super(e);
